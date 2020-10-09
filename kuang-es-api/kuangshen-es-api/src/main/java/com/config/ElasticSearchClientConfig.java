61d1b3837245ac8871ccd20b05b93b5f269b38ba
chengcheng1021 <chengcheng1021@aliyun.com>
2020-10-09 15:57:46 +0800

    es版本控制

---
+++ kuang-es-api/kuangshen-es-api/src/main/java/com/config/ElasticSearchClientConfig.java
@@ -0,0 +1,26 @@
+package com.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 狂神的 Spring 两步骤：
+ * 1、找对象
+ * 2、放到spring中待用
+ * 3、如果是springboot 就先分析源码！
+ *      xxx AutoConfiguration   xxxProperties
+ */
+@Configuration // xml -> bean
+public class ElasticSearchClientConfig {
+
+    @Bean
+    public RestHighLevelClient restHighLevelClient() {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200,"http")));
+        return client;
+    }
+
+}
--- kuang-es-api/kuangshen-es-api/src/test/java/com/kuang/KuangshenEsApiApplicationTests.java
+++ kuang-es-api/kuangshen-es-api/src/test/java/com/kuang/KuangshenEsApiApplicationTests.java
@@ -1,10 +1,15 @@
 package com.kuang;
 
+import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KuangshenEsApiApplicationTests {
+
+	@Autowired
+	private RestHighLevelClient restHighLevelClient;
 
 	@Test
 	void contextLoads() {
