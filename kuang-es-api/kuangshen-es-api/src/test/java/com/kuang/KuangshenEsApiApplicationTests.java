f9ba5b6b05ad1f3aed1728912caa717d01c07841
chengcheng1021 <chengcheng1021@aliyun.com>
2020-10-10 11:06:08 +0800

    文档的API操作，添加和获取

--- kuang-es-api/kuangshen-es-api/src/test/java/com/kuang/KuangshenEsApiApplicationTests.java
+++ kuang-es-api/kuangshen-es-api/src/test/java/com/kuang/KuangshenEsApiApplicationTests.java
@@ -1,9 +1,18 @@
 package com.kuang;
 
+import com.alibaba.fastjson.JSON;
+import com.kuang.pojo.User;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -27,6 +36,58 @@ class KuangshenEsApiApplicationTests {
 		CreateIndexResponse response = client.indices().create(request, RequestOptions.DEFAULT);
 
 		System.out.println(response);
+	}
+
+	/**
+	 * 测试添加文档
+	 */
+	@Test
+	void testAddDocument() throws IOException {
+		//1、创建对象
+		User user = new User("chengzi", 28);
+		//2、创建请求
+		IndexRequest request = new IndexRequest("kuang_index");
+
+		// 测试 put /kuang_index/_doc/1
+		request.id("1");
+		request.timeout(TimeValue.timeValueSeconds(1));
+		request.timeout("1s");
+
+		// 将我们的数据放入请求 json
+		request.source(JSON.toJSONString(user), XContentType.JSON);
+
+		// 客户端发送请求，获取响应的结果
+		IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
+
+		System.out.println(indexResponse.toString());
+		System.out.println(indexResponse.status());
+	}
+
+	/**
+	 * 测试获取文档，判断是否存在
+	 */
+	@Test
+	void testIsExists() throws IOException {
+		GetRequest getRequest = new GetRequest("kuang_index", "1");
+
+		// 不获取返回的 _source 的上下文了
+		getRequest.fetchSourceContext(new FetchSourceContext(false));
+		getRequest.storedFields("_none_");
+
+		boolean exists = client.exists(getRequest, RequestOptions.DEFAULT);
+		System.out.println(exists);
+	}
+
+	/**
+	 * 获得文档的信息
+	 */
+	@Test
+	void testGetDocument() throws IOException {
+		GetRequest getRequest = new GetRequest("kuang_index", "1");
+		GetResponse getResponse = client.get(getRequest, RequestOptions.DEFAULT);
+		// 打印文档的内容
+		System.out.println(getResponse.getSourceAsString());
+		System.out.println(getResponse);
 	}
 
 }
