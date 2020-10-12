1713dae9694f6bf7cfce02c213ce1f61d6dfaa11
chengcheng1021 <chengcheng1021@aliyun.com>
2020-10-12 19:57:07 +0800

    业务编写

---
+++ kuangshen-es-jd/src/main/java/com/kuang/service/ContentService.java
@@ -0,0 +1,47 @@
+package com.kuang.service;
+
+import com.alibaba.fastjson.JSON;
+import com.kuang.utils.HtmlParseUtil;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ContentService {
+
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
+
+    /**
+     * 不能直接使用 @Autowired，需要 Spring 容器
+     */
+    public static void main(String[] args) throws IOException {
+        new ContentService().parseContent("java");
+    }
+
+    // 1、解析数据放入 es 索引中
+    public Boolean parseContent(String keywords) throws IOException {
+        List<String> strings = new HtmlParseUtil().parseJD(keywords);
+
+        // 把查询出的数据放入 es 中
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.timeout("2m");
+        for (int i = 0; i < strings.size(); i++) {
+            bulkRequest.add(
+                    new IndexRequest("jd_goods")
+                    .source(JSON.toJSONString(strings.get(i)), XContentType.JSON));
+        }
+
+        BulkResponse bulk = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+        return !bulk.hasFailures();
+    }
+}
--- kuangshen-es-jd/src/main/java/com/kuang/utils/HtmlParseUtil.java
+++ kuangshen-es-jd/src/main/java/com/kuang/utils/HtmlParseUtil.java
@@ -8,14 +8,25 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HtmlParseUtil {
 
     public static void main(String[] args) throws IOException {
-        // 获取请求 https://search.jd.com/Search?keyword=java&enc=utf-8&wq=ja&pvid=5bab0396a85a4d3a91011eec440f61f0
+        new HtmlParseUtil().parseJD("php").forEach(System.out::println);
+    }
+
+    /**
+     * 封装
+     * @param keywords
+     * @return
+     * @throws IOException
+     */
+    public List<String> parseJD(String keywords) throws IOException {
         // 前提 联网
 
-        String url = "https://search.jd.com/Search?keyword=java&enc=utf-8&wq=ja&pvid=5bab0396a85a4d3a91011eec440f61f0";
+        String url = "https://search.jd.com/Search?keyword=" + keywords + "&enc=utf-8&wq=ja&pvid=5bab0396a85a4d3a91011eec440f61f0";
         // 解析网页。（Jsoup 返回 Document 就是浏览器返回的 Document 对象）
         Document document = Jsoup.parse(new URL(url), 30000);
         // 所以你在js中可以使用的方法，这里都能用
@@ -24,10 +35,14 @@ public class HtmlParseUtil {
 
         // 获取所有的li标签
         Elements elements = element.getElementsByTag("li");
+
+        ArrayList<String> list = new ArrayList<>();
         // 获取元素中的内容
         for (Element ele : elements) {
             String href = ele.getElementsByTag("a").eq(0).attr("href");
-            System.out.println(href);
+            list.add(href);
         }
+
+        return list;
     }
 }
