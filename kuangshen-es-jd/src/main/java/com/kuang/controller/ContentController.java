787ee00e7a19093fc2727ab1d082f9910e482f00
chengcheng1021 <chengcheng1021@aliyun.com>
2020-10-13 10:08:31 +0800

    京东搜索完成

--- kuangshen-es-jd/src/main/java/com/kuang/controller/ContentController.java
+++ kuangshen-es-jd/src/main/java/com/kuang/controller/ContentController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class ContentController {
 
@@ -15,5 +17,12 @@ public class ContentController {
     @GetMapping("/parse/{keywords}")
     public Boolean parse(@PathVariable("keywords") String keywords) throws IOException {
         return contentService.parseContent(keywords);
+    }
+
+    @GetMapping("/search/{keyword}/{pageNo}/{pageSize}")
+    public List<Map<String, Object>> search(@PathVariable("keyword") String keyword,
+                                            @PathVariable("pageNo") int pageNo,
+                                            @PathVariable("pageSize") int pageSize) throws IOException {
+        return contentService.searchPage(keyword, pageNo, pageSize);
     }
 }
--- kuangshen-es-jd/src/main/java/com/kuang/service/ContentService.java
+++ kuangshen-es-jd/src/main/java/com/kuang/service/ContentService.java
@@ -2,17 +2,29 @@ package com.kuang.service;
 
 import com.alibaba.fastjson.JSON;
 import com.kuang.utils.HtmlParseUtil;
+import org.apache.lucene.util.QueryBuilder;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.naming.directory.SearchResult;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ContentService {
@@ -28,7 +40,9 @@ public class ContentService {
 //        new ContentService().parseContent("java");
 //    }
 
-    // 1、解析数据放入 es 索引中
+    /**
+     * 1、解析数据放入 es 索引中
+     */
     public Boolean parseContent(String keywords) throws IOException {
         List<String> strings = new HtmlParseUtil().parseJD(keywords);
 
@@ -43,5 +57,39 @@ public class ContentService {
 
         BulkResponse bulk = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
         return !bulk.hasFailures();
+    }
+
+    /**
+     * 2、获取这些数据实现搜索功能
+     */
+    public List<Map<String, Object>> searchPage(String keyword, int pageNo, int pageSize) throws IOException {
+        if (pageNo <= 1) {
+            pageNo = 1;
+        }
+
+        // 条件搜索
+        SearchRequest searchRequest = new SearchRequest("jd_goods");
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+
+        // 分页
+        sourceBuilder.from(pageNo);
+        sourceBuilder.size(pageSize);
+
+        // 精确匹配
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("title", keyword);
+        sourceBuilder.query(termQueryBuilder);
+        sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+
+        // 执行搜索
+        searchRequest.source(sourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+
+        // 解析对象
+        ArrayList<Map<String, Object>> list = new ArrayList<>();
+        for (SearchHit documentFields : searchResponse.getHits().getHits()) {
+            list.add(documentFields.getSourceAsMap());
+        }
+
+        return list;
     }
 }
